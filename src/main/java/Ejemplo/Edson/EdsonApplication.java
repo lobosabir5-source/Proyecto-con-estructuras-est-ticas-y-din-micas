0fc1643e0f999af06b23cfe4f5889813323a6dd3
lobosabir5-source <tu_correo@gmail.com>
2026-05-10 19:36:44 -0400

    Estructura base del proyecto Spring Boot

---
+++ src/main/java/Ejemplo/Edson/EdsonApplication.java
@@ -0,0 +1,13 @@
+package Ejemplo.Edson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdsonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdsonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Ejemplo/Edson/EdsonApplicationTests.java
@@ -0,0 +1,13 @@
+package Ejemplo.Edson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdsonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
